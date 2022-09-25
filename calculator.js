b536a21388c95df5ccbf7719116813b3b45860ce
Prasanth K <prasanthkishore004@gmail.com>
2022-09-25 22:33:34 +0530

    removed all features

--- calculator.js
+++ calculator.js
@@ -2,36 +2,5 @@ import { LightningElement, track } from 'lwc';
 
 export default class Calculator extends LightningElement {
 
-    @track result=0;
-
-    handleAdd()
-    {
-        var num1=parseInt(this.template.querySelector('[data-id="num1"]').value);
-        var num2=parseInt(this.template.querySelector('[data-id="num2"]').value);
-        this.result=num1+num2;
- 
-    }
-
-    handleSub()
-    {
-        var num1=parseInt(this.template.querySelector('[data-id="num1"]').value);
-        var num2=parseInt(this.template.querySelector('[data-id="num2"]').value);
-        this.result=num1-num2;
-    }
-
-    handleMul()
-    {
-        var num1=parseInt(this.template.querySelector('[data-id="num1"]').value);
-        var num2=parseInt(this.template.querySelector('[data-id="num2"]').value);
-        this.result=num1*num2;
-
-    }
-    handleDiv()
-    {
-        var num1=parseInt(this.template.querySelector('[data-id="num1"]').value);
-        var num2=parseInt(this.template.querySelector('[data-id="num2"]').value);
-        this.result=num1/num2;
-
-    }
 
 }
