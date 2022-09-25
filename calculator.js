f0ca1b3bb2ce9fe6e39e1d4f772641e881c9100f
Prasanth K <prasanthkishore004@gmail.com>
2022-09-25 21:52:27 +0530

    Added ADD feature

--- calculator.js
+++ calculator.js
@@ -1,7 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Calculator extends LightningElement {
 
-    
+    @track result=0;
+
+    handleAdd()
+    {
+        var num1=parseInt(this.template.querySelector('[data-id="num1"]').value);
+        var num2=parseInt(this.template.querySelector('[data-id="num2"]').value);
+        this.result=num1+num2;
+ 
+    }
 
 }
